0e27d43e9b502eece9bd1c8d4ba0870c5f65f1ef
ebbywiselyn <ebbywiselyn@gmail.com>
2011-08-03 15:32:30 +0000

    Committed a wrong copy, checkout later revisions only

--- TweetSensor/src/tweet/sensor/TweetSensorService.java
+++ TweetSensor/src/tweet/sensor/TweetSensorService.java
@@ -18,191 +18,99 @@
 
 package tweet.sensor;
 
-import java.util.LinkedList;
+import android.app.Service;
 import android.content.Intent;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.location.Location;
-import android.location.LocationListener;
-import android.os.Bundle;
-import android.util.Log;
+import android.hardware.SensorManager;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.widget.Toast;
 
 /*
  * 
- * Class for Signal handling of sensor event listener, location listener
+ * TweetSensorService Provides sensor events for accelerometer, location
+ * Bind to the service class using bindService
+ * Start the sensor signalling by sending a message
  * 
  * @author Ebby Wiselyn	<ebbywiselyn@gmail.com>
  * @author PraveenKumar Bhadrapur <praveenkumar.bhadarpur@gmail.com>
  * @version 0.0.1
  * 
  */
-public class UserInfoHandler implements SensorEventListener, LocationListener {
+public class TweetSensorService extends Service {
 
-	private int mSampleNo = 0;
-	private int mTotalSamples = 256;
-	private double mDurationSample = 256.0;
-	private double mLongitude, mLatitude;
-	private long mStartTime = System.currentTimeMillis();
-	private long mSamplingTime[] = new long[mTotalSamples];
-	private LinkedList<Number> mAccList = new LinkedList<Number>();
-	private TweetSensorService mSensorService;
+	public static final int MSG_START_SENSING = 1;
+	public static final int MSG_STOP_SENSING = 2;
+	// Time in milliseconds
+	public static final int LOCATION_MIN_TIME_CHANGE_UPDATE = 1000;
+	// In meters
+	public static final int LOCATION_MIN_DISTANCE_CHANGE_UPDATE = 1;
+	
+	private static boolean sHandlerRegistered = false;	
+	private SensorManager mSensorMgr;
+	private LocationManager mLocationMgr;
 
-	// FIXME check if static is required
-	public static String userInfoStr;
-	private static UserInfoHandler sHandler;
+	class IncomingHandler extends Handler {
 
-	/*
-	 * Acceleration from x, y, z accelerometer values
-	 * 
-	 * @param x acceleration in x axis
-	 * 
-	 * @param y acceleration in y axis
-	 * 
-	 * @param z acceleration in z axis
-	 * 
-	 * @return double acceleration
-	 */
-	private double getAcceleration(double x, double y, double z) {
-		return Math.sqrt(x * x + y * y + z * z);
-	}
+		@Override
+		public void handleMessage(Message msg) {
 
-	/*
-	 * Singleton class implementation, Can create only one object
-	 * 
-	 * @return UserInfoHandler returns instance of UserInfoHandler
-	 */
-	public static UserInfoHandler getHandler() {
-		if (sHandler == null) {
-			sHandler = new UserInfoHandler();
-		}
+			switch (msg.what) {
 
-		return sHandler;
-	}
+			case MSG_START_SENSING:
+				Toast.makeText(getApplicationContext(), "starting sensor",
+						Toast.LENGTH_SHORT).show();
 
-	/*
-	 * Set the service instance which creates the handler, Required for calling
-	 * service specific methods like sending broadcast
-	 * 
-	 * @param sensorService service instance which creates the andler
-	 * 
-	 * @return void
-	 */
-	public void setService(TweetSensorService sensorService) {
-		this.mSensorService = sensorService;
-	}
+				mSensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+				mSensorMgr.registerListener(UserInfoHandler.getHandler(),
+						mSensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+						SensorManager.SENSOR_DELAY_NORMAL);
 
-	/*
-	 * onAccuracyChanged Callback method for change in sensor accuracy
-	 * 
-	 * @param sensor Sensor
-	 * 
-	 * @param accuracy change in accuracy
-	 */
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-	}
+				mLocationMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+				mLocationMgr.requestLocationUpdates(
+						LocationManager.GPS_PROVIDER,
+						LOCATION_MIN_TIME_CHANGE_UPDATE,
+						LOCATION_MIN_DISTANCE_CHANGE_UPDATE,
+						UserInfoHandler.getHandler());
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * onSensorChanged Callback method for change in sensor event
-	 * 
-	 * @param event SensorEvent which occurred
-	 */
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		double acc, x, y, z;
-		double linear_data[], smoothed_data[];
-		String act;
+				sHandlerRegistered = true;
+				break;
 
-		Log.d(TweetSensorService.tag, TweetSensorService.tag + "mSampleNo:"
-				+ mSampleNo);
-
-		x = event.values[0];
-		y = event.values[1];
-		z = event.values[2];
-
-		mSamplingTime[mSampleNo] = System.currentTimeMillis() - mStartTime;
-		acc = getAcceleration(x, y, z);
-		mAccList.add(acc);
-
-		// Process every mTotalSamples interval
-		if (mSampleNo >= mTotalSamples - 1) {
-
-			// Data Interpolation Linearization
-			linear_data = UserInfoUtil.linearizeData(mDurationSample,
-					mTotalSamples, mSamplingTime, mAccList);
-
-			// Smoothen the data
-			smoothed_data = UserInfoUtil.smoothenData(linear_data);
-
-			// Classify Activity
-			act = UserInfoActivityClassifier.kNNClassifyActivity(smoothed_data);
-			userInfoStr = act + "longitude:" + mLongitude + "latitude:"
-					+ mLatitude;
-
-			Log.d(TweetSensorService.tag, TweetSensorService.tag + "_Activity"
-					+ userInfoStr);
-
-			// Broadcast the data
-			if (mSensorService != null) {
-				// TODO: Use appropriate action
-				Intent i = new Intent();
-				i.putExtra("userInfoStr", userInfoStr);
-				i.setAction(Intent.ACTION_VIEW);
-				mSensorService.sendBroadcast(i);
+			case MSG_STOP_SENSING:
+				if (sHandlerRegistered) {
+					Toast.makeText(getApplicationContext(), "stopping sensor",
+							Toast.LENGTH_SHORT).show();
+					mSensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+					mSensorMgr.unregisterListener(UserInfoHandler.getHandler());
+				}
+				break;
+			default:
+				super.handleMessage(msg);
 			}
-
-			// Reset the start time
-			mStartTime = System.currentTimeMillis();
 		}
-
-		mSampleNo = mSampleNo + 1;
-		mSampleNo = mSampleNo % mTotalSamples;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * android.location.LocationListener#onLocationChanged(android.location.
-	 * Location)
-	 */
+	final Messenger mMessenger = new Messenger(new IncomingHandler());
+	static final String tag = "TweetSensor";
+
 	@Override
-	public void onLocationChanged(Location location) {
-		mLatitude = location.getLatitude();
-		mLongitude = location.getLongitude();
+	public IBinder onBind(Intent intent) {
+		Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT)
+				.show();
+
+		UserInfoHandler.getHandler().setService(this);
+		return mMessenger.getBinder();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * android.location.LocationListener#onProviderDisabled(java.lang.String)
-	 */
 	@Override
-	public void onProviderDisabled(String provider) {
-	}
+	public boolean onUnbind(Intent intent) {
+		Toast.makeText(getApplicationContext(), "unbinding", Toast.LENGTH_SHORT)
+				.show();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * android.location.LocationListener#onProviderEnabled(java.lang.String)
-	 */
-	@Override
-	public void onProviderEnabled(String provider) {
+		UserInfoHandler.getHandler().setService(null);
+		return false;
 	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.location.LocationListener#onStatusChanged(java.lang.String,
-	 * int, android.os.Bundle)
-	 */
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-	}
-
 }
